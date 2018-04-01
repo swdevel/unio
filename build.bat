0c2ef41770162926d168e84137d9f96ad2ef7ef6
Alexandr <ksandr.mobile@gmail.com>
2018-04-01 13:22:26 +0300

    Добавлен прототип скрипта сборки

---
+++ build.bat
@@ -0,0 +1,17 @@
+echo on
+
+rem build tools
+set CC=
+set NASM=
+set LD=
+set OBJCOPY=
+
+rem build directory
+set BUILD_DIR=build
+
+rem sources directories
+set BOOT_DIR=boot
+set COMMON_DIR=common
+set KERNEL_DIR=kernel 
+
+pause
