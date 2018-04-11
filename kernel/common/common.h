dc4bc1d3e890b617a0c8a420af20e9aa5ec04dfc
Alexandr <ksandr.mobile@gmail.com>
2018-04-12 00:53:35 +0300

    Добавлен заголовочный файл с макросами

---
+++ kernel/common/common.h
@@ -0,0 +1,26 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#ifndef NULL
+	#define NULL 0
+#endif
+
+#ifndef TRUE
+	#define TRUE 1
+#endif
+
+#ifndef FALSE
+	#define FALSE 0
+#endif
+
+typedef unsigned int   u32int;
+typedef int            s32int;
+typedef unsigned short u16int;
+typedef short          s16int;
+typedef unsigned char  u8int;
+typedef char           s8int;
+
+typedef unsigned char  uchar;
+typedef unsigned int   size_t;
+
+#endif /* _COMMON_H_ */
