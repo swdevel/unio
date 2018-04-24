1e49bd624c7b144d97c9bf6e7b6fcb31f7c45b36
Alexandr <ksandr.mobile@gmail.com>
2018-04-24 13:36:29 +0300

    Добавлено сканирование шины PCI и вывод на экран всех устройств

--- kernel/entry.c
+++ kernel/entry.c
@@ -7,6 +7,8 @@ void _start()
 	// Очистка экрана от сообщения загрузчика и вывод сообщения и получении контроля ядром
 	tty_clear_screen();
 	tty_put_string("kernel receive control...");
+	// Сканирование шины PCI и вывод на экран всех устройств
+	pci_scan_bus();
 
 	while (1);
 }
--- kernel/entry.h
+++ kernel/entry.h
@@ -8,4 +8,7 @@
 #include "interrupt\irq.h"
 #include "tty\tty.h"
 
+// PCI bus
+#include "pci\pci.h"
+
 #endif /* _ENTRY_H_ */
