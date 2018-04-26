5bdeeed3057b5bc0488858c28bc33624dd18964f
Alexandr <ksandr.mobile@gmail.com>
2018-04-26 23:53:59 +0300

    Частичная реализация массива, сортировка которого будет происходить при вставке и удалении элементов

---
+++ kernel/common/array.c
@@ -0,0 +1,50 @@
+﻿#include "array.h"
+#include "string.h"
+#include "../memory/heap.h"
+
+s8int array_less_than_predicate_default(type_t a, type_t b)
+{
+    return (a < b) ? TRUE : FALSE;
+}
+
+void array_init(ARRAY *array, void *addr, u32int max_size, predicate_less_than_t predicate)
+{
+    array->array = addr;
+    memset(addr, 0, max_size * sizeof(type_t));
+    array->predicate = predicate;
+    array->max_size = max_size;
+    array->size = 0;
+}
+
+void array_insert(ARRAY *array, type_t *item)
+{
+    u32int iter = 0;
+
+    ASSERT(array && item);
+    
+    if (array->size == array->max_size)
+    {
+        PANIC("The array is full!");
+    }
+
+    while (iter < array->size && array->predicate(array->array[iter], item))
+    {
+        iter++;
+    }
+
+    if (iter == array->size)
+        array->array[array->size++] = item; // Добавление элемента в конец массива
+    else
+    {
+        // Сортировка массива при вставке нового элемента
+        type_t tmp = array->array[iter];
+        while (iter < array->size)
+        {
+            iter++;
+            type_t tmp2 = array->array[iter];
+            array->array[iter] = tmp;
+            tmp = tmp2;
+        }
+        array->size++;
+    }
+}
---
+++ kernel/common/array.h
@@ -0,0 +1,28 @@
+﻿#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+#include "common.h"
+
+typedef void * type_t;
+
+/*
+    Предикат возвращает значение, отличное от нуля, если первый аргумент 
+    меньше чем второй.
+*/
+typedef s8int(*predicate_less_than_t) (type_t, type_t);
+
+typedef struct
+{
+    type_t *array;
+    u32int size;
+    u32int max_size;
+
+    predicate_less_than_t predicate; // Указатель на функцию-предикат
+} ARRAY;
+
+void array_init(ARRAY *array, void *addr, u32int max_size, predicate_less_than_t predicate);
+void array_insert(ARRAY *array, type_t *item);
+
+s8int array_less_than_predicate_default(type_t a, type_t b);
+
+#endif /* _ARRAY_H_ */
