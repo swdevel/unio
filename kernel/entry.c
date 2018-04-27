34ab2db4fbdfca6e7e32c83a3e86c33cabc9167d
Alexandr <ksandr.mobile@gmail.com>
2018-04-28 00:58:40 +0300

    Добавлено больше количество сообщений появляющихся после старта системы

--- kernel/entry.c
+++ kernel/entry.c
@@ -6,10 +6,12 @@ void _start()
     irq_init();
     // Очистка экрана от сообщения загрузчика и вывод сообщения и получении контроля ядром
     tty_clear_screen();
-    tty_put_string("kernel receive control...");
+    tty_put_string("kernel receive control...\n");
     // Сканирование шины PCI и вывод на экран всех устройств
+    tty_put_string("PCI bus devices:\n");
     pci_scan_bus();
     // Включение страничной адресации
     paging_init();
+    tty_put_string("Ready to test keyboard...\n");
     while (1);
 }
